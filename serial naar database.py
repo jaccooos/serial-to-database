1663c429cca465d2b78beb8808e1e4e6c56b9841
jaccooos <38593287+jaccooos@users.noreply.github.com>
2018-05-06 13:48:17 +0200

    werkt en is klaar

--- serial naar database.py
+++ serial naar database.py
@@ -1,56 +1,109 @@
-
-import serial 
-import MySQLdb
+import serial
+import pymysql
 import time
 
-# 'sql7.freesqldatabase.com','sql7234794','xQ8u9lhzpC','sql7234794','3306'
+device = '/dev/ttyACM0'
+baudrate = 9600
+
+serial_arduino = serial.Serial(device, baudrate, timeout=1)
 
 
-dbConn = MySQLdb.connect(host=","root","root","testing") or die ("could not connect to database")
-cursor = dbConn.cursor()
-device = '/dev/ttyUSB0'
-max=30
-start=time.time()
-while True:
+def execute_query(query):
     try:
-        print "Trying...",device 
-        arduino = serial.Serial(device, 9600) 
-    except: 
-        print "Failed to connect on",device    
+        db_conn = pymysql.connect(host="localhost",
+                                  user="monitor",
+                                  passwd="Raspberry",
+                                  db="projectbirra") or print("could not connect to database")
+        db_cursor = db_conn.cursor()
+        db_cursor.execute(query)
+        db_conn.commit()
+        db_cursor.close()
+        db_conn.close()
+    except Exception:
+        print("Error while executing query")
 
-    try: 
-        data = arduino.readline()  
-        if (data = "E")
-		{
-			try:
-            cursor.execute("INSERT INTO Statussen (Error) VALUES (%s)", (true)
-            dbConn.commit() 
-            cursor.close()  
-			except MySQLdb.IntegrityError:
-			print "failed to insert data"
-		}
-		elif (data = "M")
-		{ 
-			# lees vervolgens het getal uit 
-			
-			try:
-            cursor.execute("INSERT INTO Statussen (Magazijn) VALUES (%s)", (getal)
-            dbConn.commit() 
-            cursor.close()  
-			except MySQLdb.IntegrityError:
-			print "failed to insert data"
-		}
-		pieces = data.split("\t")  
-		
-        
-        #finally:
-            #cursor.close()
-    except:
-        print "Failed to get data from Arduino!"
-    time.sleep(10)
-    print "looping..."
-    remaining=max+start-time.time()
-    print "%s seconds remaining" % int(remaining)
-    if remaining<=0:
-        cursor.close()
-        break
+
+def get_first_order(status):
+    try:
+        db_conn = pymysql.connect(host="localhost",
+                                  user="monitor",
+                                  passwd="Raspberry",
+                                  db="projectbirra") or print("could not connect to database")
+        db_cursor = db_conn.cursor()
+        row_count = db_cursor.execute("SELECT idBestelling FROM bestelling WHERE Status = " + str(status) + " LIMIT 1")
+
+        if row_count > 0: # If there are more then 0 rows
+            row = db_cursor.fetchone()
+            db_cursor.close()
+            db_conn.close()
+            return row[0]
+
+        db_cursor.close()
+        db_conn.close()
+        return -1
+
+    except Exception:
+        print("Error while executing query")
+        return -1
+
+
+while True:
+    # Wait for 1 second on data
+    try:
+        data = str(serial_arduino.readline(), 'ascii')
+    except UnicodeDecodeError:
+        data = ""
+        print("Can't decode data")
+
+    if len(data) > 0:
+        print("Data received: ", data)
+
+        if data[0] == "d":  # Done
+            id_order = get_first_order(2)  # Pak eerste bestelling die bezig is
+            if id_order != -1:
+                execute_query("UPDATE bestelling SET Status=3 WHERE idBestelling=" + str(id_order))
+
+        elif data[0] == "u":  # Uitstoten
+            id_order = get_first_order(1)  # Pak eerste verzonden bestelling in queue
+            if id_order != -1:
+                execute_query("UPDATE bestelling SET Status=2 WHERE idBestelling=" + str(id_order))
+
+        elif data[0] == "t":  # Tappen
+            pass
+        elif data[0] == "g":  # Draaien
+            pass
+        elif data[0] == "o":  # Opstarten
+            execute_query("DELETE FROM bestelling")  # Verwijder alle bestellingen
+
+        elif data[0] == "e":  # Error
+            execute_query("UPDATE Statussen SET Error=1, Bezig=0, Gereed=0")
+            id_order = get_first_order(2)  # Pak eerste bestelling die bezig is
+            if id_order != -1:
+                execute_query("UPDATE bestelling SET Status=1 WHERE idBestelling=" + str(id_order))
+
+        elif data[0] == "s":  # Standby
+            pass
+        elif data[0] == "c":  # Celcius
+            temperature = data[1:]
+            execute_query("UPDATE Statussen SET Temperatuur=" + temperature)
+
+    print("Checking database..")
+    id_order = get_first_order(0)
+    if id_order != -1:  # New order available
+        print("Adding order", id_order, "to queue")
+        serial_arduino.write('b'.encode())
+        time.sleep(0.1)
+
+        # Check if R received
+        try:
+            data = ""
+            data = str(serial_arduino.readline(), 'ascii')
+
+            if len(data) > 0 and data[0] == 'r':
+                execute_query("UPDATE bestelling SET Status=1 WHERE idBestelling=" + str(id_order))
+        except UnicodeDecodeError:
+            data = ""
+            print("Can't decode data")
+
+
+
